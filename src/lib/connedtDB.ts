5498647bd91a0a39803c3edbe1f45a3ea6083b07
vishalprajapati15 <vishalprajapati3266@gmail.com>
2026-06-20 20:20:45 +0530

    DB connection Done

---
+++ src/lib/connedtDB.ts
@@ -0,0 +1,34 @@
+import mongoose from "mongoose";
+
+const mongoDbUri = process.env.MONGODB_URI;
+
+if (!mongoDbUri) {
+    throw new Error("MongoDb Uri Error!!");
+}
+
+let cached = global.mongoose
+
+if (!cached) {
+    cached = global.mongoose = { conn: null, promise: null };
+}
+
+const connectDB = async () => {
+    if (cached.conn) {
+        return cached.conn;
+    }
+
+    if (!cached.promise) {
+        cached.promise = mongoose.connect(mongoDbUri).then((conn) => conn.connection)
+    }
+
+    try {
+        const conn = await cached.promise
+        return conn;
+
+
+    } catch (error) {
+        console.log('DB Connection Error : ', error);
+    }
+}
+
+export default connectDB;
---
+++ src/model/user.model.ts
@@ -0,0 +1,112 @@
+import mongoose from "mongoose";
+
+
+interface IUser {
+    id?:mongoose.Types.ObjectId;
+    name:string;
+    email: string;
+    password?: string;
+    image?:string;
+    phone?:string;
+    role:"user" | "vendor" | "admin";
+
+    shopName?:string;
+    shopAddress?:string;
+    gstNumber?:string;
+    isApproved?: boolean;
+    verificationStatus?:  "pending" | "approved" | "rejected";
+    requestAt?: Date;
+    approvedAt?: Date;
+    rejectedReason?: string;
+    vecdorProducts?: mongoose.Types.ObjectId[];
+    orders?: mongoose.Types.ObjectId[];
+
+    cart?:{
+        product: mongoose.Types.ObjectId;
+        quantity: number;
+    }[]
+
+    createdAt?: Date;
+    updatedAt?: Date;
+
+
+
+}
+
+
+const userSchema = new mongoose.Schema<IUser>({
+    name:{
+        type: String,
+        required: true,
+    },
+    email:{
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password:{
+        type: String,
+    },
+    image:{
+        type: String,
+    },
+    phone:{
+        type: String,
+    },
+    role:{
+        type: String,
+        enum: ["user", "vendor", "admin"],
+        required: true,
+        default: "user",
+    },
+    shopName:{
+        type: String,
+    },
+    shopAddress:{
+        type: String,
+    },
+    gstNumber:{
+        type:String,
+    },
+    isApproved: {
+        type: Boolean,
+        default: false,
+    },
+    verificationStatus:{
+        type: String,
+        enum: ["pending", "approved", "rejected"],
+        default: "pending",
+    },
+    approvedAt: {
+        type: Date
+    },
+    requestAt: {
+        type: Date
+    },
+    rejectedReason:{
+        type: String
+    },
+    vecdorProducts:[{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Product"
+    }],
+    orders:[{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Product"
+    }],
+    cart:[{
+        product:{
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Product"
+        },
+        quantity:{
+            type: Number,
+            default: 1,
+        }
+    }   
+    ]
+
+}, {timestamps: true});
+
+const User = mongoose.models?.User || mongoose.model<IUser>("User", userSchema);
+export default User;
